231ab42b22e720f62d31dd8e724d771240e1f8e0
David Juárez <3300256001201@ingenieria.usac.edu.gt>
2018-02-22 20:29:38 -0600

    Diseño interfaz

---
+++ pc1_lenguajes/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace pc1_lenguajes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaPestañaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compilaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.análisisLéxicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buscarCoincidenciasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.archivoDeSalidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manualDeUsuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manualTécnicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabEditor = new System.Windows.Forms.TabControl();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.compilaciónToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1128, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaPestañaToolStripMenuItem,
+            this.cargarArchivoToolStripMenuItem,
+            this.guardarArchivoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevaPestañaToolStripMenuItem
+            // 
+            this.nuevaPestañaToolStripMenuItem.Name = "nuevaPestañaToolStripMenuItem";
+            this.nuevaPestañaToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.nuevaPestañaToolStripMenuItem.Text = "Nueva pestaña";
+            this.nuevaPestañaToolStripMenuItem.Click += new System.EventHandler(this.nuevaPestañaToolStripMenuItem_Click);
+            // 
+            // cargarArchivoToolStripMenuItem
+            // 
+            this.cargarArchivoToolStripMenuItem.Name = "cargarArchivoToolStripMenuItem";
+            this.cargarArchivoToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.cargarArchivoToolStripMenuItem.Text = "Cargar archivo";
+            this.cargarArchivoToolStripMenuItem.Click += new System.EventHandler(this.cargarArchivoToolStripMenuItem_Click);
+            // 
+            // guardarArchivoToolStripMenuItem
+            // 
+            this.guardarArchivoToolStripMenuItem.Name = "guardarArchivoToolStripMenuItem";
+            this.guardarArchivoToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.guardarArchivoToolStripMenuItem.Text = "Guardar archivo";
+            this.guardarArchivoToolStripMenuItem.Click += new System.EventHandler(this.guardarArchivoToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // compilaciónToolStripMenuItem
+            // 
+            this.compilaciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.análisisLéxicoToolStripMenuItem,
+            this.buscarCoincidenciasToolStripMenuItem,
+            this.archivoDeSalidaToolStripMenuItem});
+            this.compilaciónToolStripMenuItem.Name = "compilaciónToolStripMenuItem";
+            this.compilaciónToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.compilaciónToolStripMenuItem.Text = "Compilación";
+            // 
+            // análisisLéxicoToolStripMenuItem
+            // 
+            this.análisisLéxicoToolStripMenuItem.Enabled = false;
+            this.análisisLéxicoToolStripMenuItem.Name = "análisisLéxicoToolStripMenuItem";
+            this.análisisLéxicoToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.análisisLéxicoToolStripMenuItem.Text = "Análisis léxico";
+            // 
+            // buscarCoincidenciasToolStripMenuItem
+            // 
+            this.buscarCoincidenciasToolStripMenuItem.Enabled = false;
+            this.buscarCoincidenciasToolStripMenuItem.Name = "buscarCoincidenciasToolStripMenuItem";
+            this.buscarCoincidenciasToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.buscarCoincidenciasToolStripMenuItem.Text = "Buscar coincidencias";
+            // 
+            // archivoDeSalidaToolStripMenuItem
+            // 
+            this.archivoDeSalidaToolStripMenuItem.Enabled = false;
+            this.archivoDeSalidaToolStripMenuItem.Name = "archivoDeSalidaToolStripMenuItem";
+            this.archivoDeSalidaToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.archivoDeSalidaToolStripMenuItem.Text = "Archivo de salida";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manualDeUsuarioToolStripMenuItem,
+            this.manualTécnicoToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // manualDeUsuarioToolStripMenuItem
+            // 
+            this.manualDeUsuarioToolStripMenuItem.Name = "manualDeUsuarioToolStripMenuItem";
+            this.manualDeUsuarioToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.manualDeUsuarioToolStripMenuItem.Text = "Manual de usuario";
+            // 
+            // manualTécnicoToolStripMenuItem
+            // 
+            this.manualTécnicoToolStripMenuItem.Name = "manualTécnicoToolStripMenuItem";
+            this.manualTécnicoToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.manualTécnicoToolStripMenuItem.Text = "Manual técnico";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de...";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(815, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coincidencias";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(818, 67);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(298, 327);
+            this.textBox2.TabIndex = 3;
+            // 
+            // tabEditor
+            // 
+            this.tabEditor.Location = new System.Drawing.Point(13, 28);
+            this.tabEditor.Name = "tabEditor";
+            this.tabEditor.SelectedIndex = 0;
+            this.tabEditor.Size = new System.Drawing.Size(795, 497);
+            this.tabEditor.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 537);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabEditor);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Plagiacaco";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevaPestañaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compilaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem análisisLéxicoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buscarCoincidenciasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem archivoDeSalidaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manualDeUsuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manualTécnicoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabControl tabEditor;
+    }
+}
+
---
+++ pc1_lenguajes/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pc1_lenguajes
+{
+    public partial class Form1 : Form
+    {
+        int noPestania = 0;
+        TabPage nueva;
+        TextBox tb;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result1 = MessageBox.Show("¿Está seguro que desea abandonar el programa?", "Atención!", MessageBoxButtons.YesNo);
+            if (result1 == DialogResult.Yes)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+        }   
+
+        private void nuevaPestañaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            crearPestana();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void crearPestana()
+        {
+            nueva = new TabPage();
+            tb = new TextBox();
+
+            //Configuracion de la Pestaña
+            tabEditor.TabPages.Add(nueva);
+            nueva.Text = "Archivo" + noPestania++;
+            nueva.Name = "tab"+ noPestania;
+
+            //Configuracion del textbox correspondiente a la pestaña
+            tb.Name = "tab" + noPestania;
+            tb.Multiline = true;
+            tb.ScrollBars = ScrollBars.Vertical;
+            tb.Size = nueva.Size;
+
+            nueva.Controls.Add(tb);
+
+            if (tabEditor.TabCount >= 2)
+            {
+                análisisLéxicoToolStripMenuItem.Enabled = true;
+            }
+            tabEditor.SelectedTab = nueva;
+        }
+
+        private void guardarArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String texto ="";
+            // Dialogo para guardar archivo
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Archivo de texto|*.txt";
+            saveFileDialog1.Title = "Guardar archivo";
+            //Obtiene el texto de la pestaña en la que estamos trabajando
+            if (tabEditor.SelectedTab.Controls.ContainsKey(tabEditor.SelectedTab.Name))
+            {
+                texto = tabEditor.SelectedTab.Controls[tabEditor.SelectedTab.Name].Text;
+            }
+            saveFileDialog1.ShowDialog();
+            //Guarda el archivo siempre que ni el nombre del archivo o el textbox este vacio
+            if ((saveFileDialog1.FileName != "") && (texto != null))
+            {
+                    try
+                {
+
+                    //Nombre y ruta del archivo
+                    StreamWriter escritor = new StreamWriter(saveFileDialog1.FileName);
+                    
+                    //escribe el texto
+                    escritor.Write(texto);
+
+                    //Cerramos el archivo
+                    escritor.Close();
+
+                    tabEditor.SelectedTab.Text = Path.GetFileNameWithoutExtension(saveFileDialog1.FileName);
+                }
+                //Excepciones
+                catch (Exception fe)
+                {
+                    Console.WriteLine("Exception: " + fe.Message);
+                }
+                finally
+                {
+                }
+            }
+        }
+
+        private void cargarArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Archivo de texto|*.txt";
+            openFileDialog1.Title = "Cargar Archivo";
+            
+            String texto = "";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                texto = sr.ReadToEnd();
+                sr.Close();
+                    crearPestana();
+                    tabEditor.SelectedTab.Controls["tab" + noPestania].Text = texto;
+                    tabEditor.SelectedTab.Text = Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+            }
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("César David Juárez González \n 201503515 \n Universidad de San Carlos de Guatemala \n Facultad de Ingeniería");
+        }
+    }
+}
